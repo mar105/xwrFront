02389c219338b682744c79c342fa29a2c08fcb45
yanghenglin <yanghl@xlyerp.com>
2021-01-28 23:11:26 +0800

    解决js到ts的报错。

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = true; //process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, './src/index.tsx'),
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bunlde.js'
+    filename: '[name].[hash].js'
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -23,29 +26,28 @@ module.exports = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/react', '@babel/preset-env', '@babel/preset-typescript']
+          presets: ['@babel/react', '@babel/preset-env', '@babel/preset-typescript'],
+          // plugins: [
+          //   ['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}], // `style: true` 会加载 less 文件
+          // ],
         }
       },
-      // loader: 'babel-loader',
-      // options: {
-      //   presets: [
-      //     '@babel/preset-env',
-      //     '@babel/preset-react',
-      //     '@babel/preset-typescript'
-      //   ],
-      //   plugins: [
-      //     ['import', { libraryName: 'antd', style: 'css' }], // `style: true` 会加载 less 文件
-      //   ],
-      // }
     },
-    {
-      test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-    }
-  ]},
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader']
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new htmlWebpackPlugin({
-    template: './src/index.html'
-  })]
+      hash: true,
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    })
+  ]
 };

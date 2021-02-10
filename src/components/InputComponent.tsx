360dc9d7a0b9a475c876a7f267fee77a38adc141
yanghenglin <yanghl@xlyerp.com>
2021-02-10 10:40:27 +0800

    Form表单例子功能。

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,42 +1,18 @@
 import React from 'react';
 import { Form, Input } from 'antd';
 import { componentType } from '../utils/commonTypes';
-import * as commonUtils from '../utils/commonUtils';
-// export type paramProps = {
-//   form: any,
-//   fieldName: string,
-//   property: string,
-//   rules: object,
-// }
-//
-// const InputComponent: React.FC<paramProps> = (params: paramProps) => {
-//   const { getFieldDecorator } = params.form;
-//   return (
-//     getFieldDecorator(params.fieldName, { rules: [{ ...params.rules }] })(
-//       <Input id={params.fieldName} {...params.property} />)
-// );
-//
-// }
-// export default InputComponent;
-
-function InputFieldDecoratorComponent(params) {
-  const { current } = params.form;
-  if (commonUtils.isNotEmpty(current)) {
-    return (
-      current.getFieldDecorator(params.fieldName, { rules: [{ ...params.rules }] })(
-        <Input id={params.fieldName} {...params.property} />)
-    );
-  } else {
-    return <Input {...params.property} />;
-  }
-
-}
 
 export function InputComponent(params) {
-  if (params.componentType === componentType.FieldDecorator) {
-    return InputFieldDecoratorComponent(params);
-  } if (params.componentType === componentType.Soruce) {
+  if (params.componentType === componentType.Soruce) {
     return <Input {...params.property} />;
   }
-  return <Form.Item>{InputFieldDecoratorComponent(params)}</Form.Item>;
+  return <Form.Item
+    label={params.label}
+    name={params.fieldName}
+    rules={params.rules}
+    shouldUpdate={(prevValues, currentValues) => { return prevValues[params.fieldName] !== currentValues[params.fieldName]
+  }
+  }>
+    <Input {...params.property} />
+  </Form.Item>;
 }
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,38 +1,46 @@
-import React, { Component } from 'react';
-import { Form } from 'antd';
+import React from 'react';
+import { Form, Button } from 'antd';
 import { connect } from 'dva';
 import { InputComponent } from "../components/InputComponent";
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        token: '',
-    };
-  }
-  handleFinish = (e) => {
-    console.log('ee', e);
-  }
-
-  render() {
-    const form: any = React.createRef();
-    const userName = {
-      form,
-      fieldName: 'userName',
-      rules: {
-        required: true,
-        message: '请输入你的用户名',
-      },
-      property: {
-        placeholder: '请输入你的用户名',
-      },
-    };
-    return (
-        <Form ref={form}>
-          <InputComponent {...userName} />
-        </Form>
-    );
-  }
+const  Register = () => {
+  const [form] = Form.useForm();
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+  const userName = {
+    form,
+    fieldName: 'userName',
+    rules: [{ required: true, message: '请输入你的用户名' }],
+    property: { placeholder: '请输入你的用户名' },
+  };
+  const userName1 = {
+    form,
+    fieldName: 'userName1',
+    rules: [{ required: true, message: '请输入你的用户名' }],
+    property: { placeholder: '请输入你的用户名' },
+  };
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+  return (
+      <Form {...layout} name="basic" form={form} onFinishFailed={onFinishFailed} onFinish={onFinish}>
+        <InputComponent {...userName} />
+        <InputComponent {...userName1} />
+        <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        </Form.Item>
+      </Form>
+  );
 }
 
 export default connect()(Register);

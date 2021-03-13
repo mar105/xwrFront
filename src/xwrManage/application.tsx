ae49be9521b7bf679981078fcdfca74c820f57f9
yanghenglin <yanghl@xlyerp.com>
2021-03-13 16:24:00 +0800

    管理页面登录修改。

---
+++ src/xwrManage/application.tsx
@@ -0,0 +1,3 @@
+export const prefix: string = 'xwrManage';
+export const pageSize: number = 20;
+export const urlPrefix: string = 'http://127.0.0.1:8202/xwrManage';
---
+++ src/xwrManage/models/commonModel.tsx
@@ -0,0 +1,40 @@
+import { routerRedux } from 'dva/router';
+import * as application from '../application';
+
+export default {
+  namespace: 'commonModel',
+  state: {
+    token: localStorage.getItem(`${application.prefix}token`) || '',
+    userInfo: localStorage.getItem(`${application.prefix}userInfo`) || '',
+    provinceCityArea: localStorage.getItem(`${application.prefix}provinceCityArea`) || [],
+  },
+  reducers: {
+    saveToken(state, { payload: token }) {
+      localStorage.setItem(`${application.prefix}token`, token);
+      return { ...state, token };
+    },
+    saveUserInfo(state, { payload: userInfo }) {
+      localStorage.setItem(`${application.prefix}userInfo`, userInfo);
+      return { ...state, userInfo };
+    },
+    saveProvinceCityArea(state, { payload: provinceCityArea }) {
+      localStorage.setItem(`${application.prefix}provinceCityArea`, provinceCityArea);
+      return { ...state, provinceCityArea };
+    },
+  },
+  effects: {
+    * gotoNewPage({ payload }, { put }) {
+      yield put(routerRedux.push(payload.newPage));
+    },
+    * gotoError({ payload }, { put }) {
+      const { code, msg } = payload.interfaceReturn;
+      console.log('payload', msg, code);
+      if (code === 'login') {
+        yield put(routerRedux.push('login'));
+        throw new Error(msg);
+      } else {
+        throw new Error(msg);
+      }
+    },
+  },
+};

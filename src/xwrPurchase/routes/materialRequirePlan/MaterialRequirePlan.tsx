19117e1eed0db885d48cb569641990cc4c4500c3
mar105 <mar105@126.com>
2022-04-04 09:45:15 +0800

    报错处理。

--- src/xwrPurchase/routes/materialRequirePlan/MaterialRequirePlan.tsx
+++ src/xwrPurchase/routes/materialRequirePlan/MaterialRequirePlan.tsx
@@ -15,7 +15,7 @@ const CommonList = (props) => {
 
   const onRowSelectChange = (name, selectedRowKeys, selectedRows) => {
     const { dispatchModifyState, [name + 'Container']: container, [name + 'Data']: tableData, [name + 'SelectedRowKeys']: selectedRowKeysOldOld } = props;
-    const returnData = props.onRowSelectChange(name, selectedRowKeys, selectedRows);
+    const returnData = props.onRowSelectChange(name, selectedRowKeys, selectedRows, true);
     const addState: any = {};
     const superiorQtyData: any = [];
     if (commonUtils.isNotEmptyArr(props.containerData)) {
@@ -25,60 +25,59 @@ const CommonList = (props) => {
         if (containerIndex > -1) {
           const superiorDataSetName = props.containerData[containerIndex].dataSetName;
           const superiorData = commonUtils.isEmptyArr(props[superiorDataSetName + 'Data']) ? [] : [...props[superiorDataSetName + 'Data']];
-          const superiorSelectRowKeys: any = [];
-          const superiorSelectRows: any = [];
-          superiorData.forEach((superior, index) => {
-            superiorData[index] = {...superior,
-              unMeasureQty: superior.benchmarkMeasureQty,
-              unMaterialQty: superior.benchmarkMaterialQty,
-              unConvertQty: superior.benchmarkConvertQty
-            };
+          superiorData.forEach((superior) => {
+            superior.unMeasureQty = superior.benchmarkMeasureQty;
+            superior.unMaterialQty = superior.benchmarkMaterialQty;
+            superior.unConvertQty = superior.benchmarkConvertQty;
+            // superiorData[index] = {...superior,
+            //   unMeasureQty: superior.benchmarkMeasureQty,
+            //   unMaterialQty: superior.benchmarkMaterialQty,
+            //   unConvertQty: superior.benchmarkConvertQty
+            // };
           });
           selectedRowKeys.forEach(selectedRowKey => {
             const index = tableData.findIndex(item => item[container.tableKey] === selectedRowKey);
             if (index > -1) {
               //求和嵌套表
               const superiorIndex = superiorData.findIndex(item => item[container.treeKey] === tableData[index][container.treeSlaveKey]);
-              const qtyIndex = superiorQtyData.findIndex(item => item[container.treeSlaveKey] === tableData[index][container.treeSlaveKey]);
-              if (qtyIndex > -1) {
-                superiorData[superiorIndex] = {...superiorData[superiorIndex],
-                  unMeasureQty: superiorData[superiorIndex].unMeasureQty + tableData[index].unMeasureQty,
-                  unMaterialQty: superiorData[superiorIndex].unMaterialQty + tableData[index].unMaterialQty,
-                  unConvertQty: superiorData[superiorIndex].unConvertQty + tableData[index].unConvertQty
+              if (superiorIndex > -1) {
+                const qtyIndex = superiorQtyData.findIndex(item => item[container.treeSlaveKey] === tableData[index][container.treeSlaveKey]);
+                if (qtyIndex > -1) {
+                  superiorData[superiorIndex] = {...superiorData[superiorIndex],
+                    unMeasureQty: superiorData[superiorIndex].unMeasureQty + tableData[index].unMeasureQty,
+                    unMaterialQty: superiorData[superiorIndex].unMaterialQty + tableData[index].unMaterialQty,
+                    unConvertQty: superiorData[superiorIndex].unConvertQty + tableData[index].unConvertQty
+                  };
+
+                  superiorQtyData[qtyIndex] = {
+                    [container.treeSlaveKey]: tableData[index][container.treeSlaveKey],
+                    unMeasureQty: superiorQtyData[qtyIndex].unMeasureQty + tableData[index].unMeasureQty,
+                    unMaterialQty: superiorQtyData[qtyIndex].unMaterialQty + tableData[index].unMaterialQty,
+                    unConvertQty: superiorQtyData[qtyIndex].unConvertQty + tableData[index].unConvertQty,
+                  };
+                } else {
+                  superiorData[superiorIndex] = {...superiorData[superiorIndex],
+                    unMeasureQty: tableData[index].unMeasureQty,
+                    unMaterialQty: tableData[index].unMaterialQty,
+                    unConvertQty: tableData[index].unConvertQty
+                  };
+
+                  superiorQtyData.push({[container.treeSlaveKey]: tableData[index][container.treeSlaveKey],
+                    unMeasureQty: tableData[index].unMeasureQty,
+                    unMaterialQty: tableData[index].unMaterialQty,
+                    unConvertQty: tableData[index].unConvertQty
+                  });
                 }
-
-                superiorQtyData[qtyIndex] = {
-                  [container.treeSlaveKey]: tableData[index][container.treeSlaveKey],
-                  unMeasureQty: superiorQtyData[qtyIndex].unMeasureQty + tableData[index].unMeasureQty,
-                  unMaterialQty: superiorQtyData[qtyIndex].unMaterialQty + tableData[index].unMaterialQty,
-                  unConvertQty: superiorQtyData[qtyIndex].unConvertQty + tableData[index].unConvertQty,
-                };
-              } else {
-                superiorData[index] = {...superiorData[superiorIndex],
-                  unMeasureQty: tableData[index].unMeasureQty,
-                  unMaterialQty: tableData[index].unMaterialQty,
-                  unConvertQty: tableData[index].unConvertQty
-                };
-
-                superiorQtyData.push({[container.treeSlaveKey]: tableData[index][container.treeSlaveKey],
-                  unMeasureQty: tableData[index].unMeasureQty,
-                  unMaterialQty: tableData[index].unMaterialQty,
-                  unConvertQty: tableData[index].unConvertQty
-                });
               }
+
             }
           });
           addState[superiorDataSetName + 'Data'] = superiorData;
-          addState[superiorDataSetName + 'SelectedRowKeys'] = superiorSelectRowKeys;
-          addState[superiorDataSetName + 'SelectRows'] = superiorSelectRows;
         }
       } else {
         //勾选父级表
         const containerIndex = props.containerData.findIndex(item => item.superiorContainerId === container.id);
         if (containerIndex > -1) {
-          const nestDataSetName = props.containerData[containerIndex].dataSetName;
-          const nestSelectRowKeys: any = commonUtils.isEmptyArr(props[nestDataSetName + 'SelectedRowKeys']) ? [] : [...props[nestDataSetName + 'SelectedRowKeys']];
-          const nestSelectRows: any = commonUtils.isEmptyArr(props[nestDataSetName + 'SelectedRows']) ? [] : [...props[nestDataSetName + 'SelectedRows']];
           const selectedRowKeysOld = commonUtils.isEmptyArr(selectedRowKeysOldOld) ? [] : selectedRowKeysOldOld;
 
           const filterKeys = selectedRowKeysOld.filter(item => !selectedRowKeys.includes(item));
@@ -109,12 +108,9 @@ const CommonList = (props) => {
             });
           }
           addState[name + 'Data'] = tableData;
-          addState[nestDataSetName + 'SelectedRowKeys'] = nestSelectRowKeys;
-          addState[nestDataSetName + 'SelectRows'] = nestSelectRows;
         }
       }
     }
-
     dispatchModifyState({ ...returnData, ...addState });
   }
 

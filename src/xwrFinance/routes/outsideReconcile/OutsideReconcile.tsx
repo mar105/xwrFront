31dd946255478fb7c93db4f58ba812eade0e6fe2
mar105 <mar105@126.com>
2022-04-22 20:20:28 +0800

    外协对账审核功能。

---
+++ src/xwrFinance/routes/outsideReconcile/OutsideReconcile.tsx
@@ -0,0 +1,59 @@
+import { connect } from 'dva';
+import React, {useMemo} from 'react';
+import {Col, Form, Row, Tooltip} from "antd";
+import commonBase from "../../../common/commonBase";
+import * as commonUtils from "../../../utils/commonUtils";
+import {ButtonGroup} from "../../../common/ButtonGroup";
+import commonDocEvent from "../../../common/commonDocEvent";
+import { CommonExhibit } from "../../../common/CommonExhibit";
+import {TableComponent} from "../../../components/TableComponent";
+import { DeleteOutlined } from '@ant-design/icons';
+import CommonModal from "../../../common/commonModal";
+import commonBillEvent from "../../../common/commonBillEvent";
+import commonPurchaseEvent from "../../../common/commonPurchaseEvent";
+
+const OutsideReconcile = (props) => {
+  const [form] = Form.useForm();
+  props.onSetForm(form);
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const { enabled, masterContainer, masterData, commonModel } = props;
+  const buttonGroup = { userInfo: commonModel.userInfo, onClick: props.onButtonClick, enabled, permissionData: props.permissionData, container: masterContainer,
+    isModal: props.isModal, buttonGroup: props.getButtonGroup() };
+  const slaveParam: any = commonUtils.getTableProps('slave', props);
+  slaveParam.isDragRow = true;
+  slaveParam.pagination = false;
+  slaveParam.width = 2000;
+  slaveParam.lastColumn = { title: 'o', changeValue: commonUtils.isEmptyObj(masterData) ? '' : masterData.defaultslaveId,
+    render: (text,record, index)=> {
+    return <div>
+      <a onClick={props.onLastColumnClick.bind(this, 'slave', 'delButton', record)}> <Tooltip placement="top" title="删除"><DeleteOutlined /> </Tooltip></a>
+    </div>
+  }, width: 50 , fixed: 'right' };
+
+  const component = useMemo(()=>{ return (
+    <CommonExhibit name="master" {...props} />)}, [masterContainer, masterData, enabled]);
+  return (
+    <div>
+      <Form {...layout} name="basic" form={form} onFinish={props.onFinish}>
+        <Row>
+          <Col>
+            {component}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {commonUtils.isNotEmptyObj(props.slaveContainer) ? <TableComponent {...slaveParam} /> : '' }
+          </Col>
+        </Row>
+        <ButtonGroup {...buttonGroup} />
+      </Form>
+      <CommonModal {...props} />
+    </div>
+  );
+}
+
+export default connect(commonUtils.mapStateToProps)(commonBase(commonDocEvent(commonBillEvent(commonPurchaseEvent(OutsideReconcile)))));

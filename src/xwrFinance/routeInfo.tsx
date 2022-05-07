1952ef4e0cdb90597771809e464f64abeab80a24
mar105 <mar105@126.com>
2022-05-07 20:43:21 +0800

    统一相同界面。

--- src/xwrFinance/routeInfo.tsx
+++ src/xwrFinance/routeInfo.tsx
@@ -7,12 +7,8 @@ export const routeInfo: any[] = [
       path: '/xwrFinance/initSupply', title: '供应商期初', name: 'initCustomer', component: () => import('./routes/initSupply/InitSupply'),
     }, {
       path: '/xwrFinance/initFinance', title: '财务期初', name: 'initFinance', component: () => import('./routes/initFinance/InitFinance'),
-    }, {
-      path: '/xwrFinance/saleReconcile', title: '销售对账', name: 'saleReconcile', component: () => import('./routes/saleReconcile/SaleReconcile'),
     },{
       path: '/xwrFinance/saleInvoice', title: '销售发票', name: 'saleInvoice', component: () => import('./routes/saleInvoice/SaleInvoice'),
-    }, {
-      path: '/xwrFinance/purchaseReconcile', title: '采购对账', name: 'purchaseReconcile', component: () => import('./routes/purchaseReconcile/PurchaseReconcile'),
     },{
       path: '/xwrFinance/purchaseInvoice', title: '采购发票', name: 'purchaseInvoice', component: () => import('./routes/purchaseInvoice/PurchaseInvoice'),
     }]

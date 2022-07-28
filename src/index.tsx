9977e0211603f5dfdb8238b77ffb2961a70950e1
xulinyu <xlyhacker@gmail.com>
2022-07-28 22:20:00 +0800

    优化

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@
  * @Author: xulinyu xlyhacker@gmail.com
  * @Date: 2022-07-25 21:50:19
  * @LastEditors: xulinyu xlyhacker@gmail.com
- * @LastEditTime: 2022-07-25 22:44:35
+ * @LastEditTime: 2022-07-28 22:18:35
  * @FilePath: \xwrFront\src\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import dva from 'dva';
 import { createBrowserHistory } from 'history';
 import { message } from 'antd';
-import './index.less';
 import 'antd/dist/antd.less';
-import './assets/less/index.less'
 
 // 1. Initialize
 const app = dva({
